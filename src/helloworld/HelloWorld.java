f4c57d81b88b88652c156f30de36757bf7986e33
Arish <vermaris@sheridancollege.ca>
2023-05-18 14:16:02 -0400

    my first commit

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -14,6 +14,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello");
         // TODO code application logic here
     }
     
