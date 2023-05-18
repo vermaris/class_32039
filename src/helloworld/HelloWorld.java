2a3b822aae0708b90b59d48d7710567f5294be27
vermaris <134000606+vermaris@users.noreply.github.com>
2023-05-18 14:57:57 -0400

    Update HelloWorld.java

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorld {
         System.out.println("hello");
         System.out.println("welcome to demo of GitHub");
         // TODO code application logic here
+        // this file is ready fro fetch and merge
     }
     
 }
