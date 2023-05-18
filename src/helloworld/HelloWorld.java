069258c2cef53b8fdbf33948cee31a442cdf139a
vermaris <134000606+vermaris@users.noreply.github.com>
2023-05-18 14:54:20 -0400

    Update HelloWorld.java
    print line added

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,6 +15,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("welcome to demo of GitHub");
         // TODO code application logic here
     }
     
